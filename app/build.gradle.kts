44ed6e386dea6d7cd2af203fefaa1980ec92b144
Mihneas22 <mihnea.serban06@gmail.com>
2024-02-18 23:10:06 +0200

    Update build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -87,4 +87,13 @@ dependencies {
     //Realm
     implementation("io.realm.kotlin:library-base:1.11.0")
 
+    // Retrofit
+    implementation("com.squareup.retrofit2:retrofit:2.9.0")
+    implementation("com.squareup.retrofit2:converter-gson:2.9.0")
+    implementation("com.squareup.okhttp3:okhttp:4.11.0")
+    implementation("com.squareup.okhttp3:logging-interceptor:4.10.0")
+
+    // Coil
+    implementation("io.coil-kt:coil-compose:2.4.0")
+
 }
