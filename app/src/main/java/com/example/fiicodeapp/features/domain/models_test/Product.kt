de4b0e08a25d8a64c3685ec17ba592cb3688b542
Mihneas22 <mihnea.serban06@gmail.com>
2024-02-19 23:39:08 +0200

    ModelTest

---
+++ app/src/main/java/com/example/fiicodeapp/features/domain/models_test/Product.kt
@@ -0,0 +1,8 @@
+package com.example.fiicodeapp.features.domain.models_test
+
+data class Product(
+    val nutriments: Nutriments,
+    val nutriscore_data: NutriscoreData,
+    val nutrition_grades: String,
+    val product_name: String
+)
