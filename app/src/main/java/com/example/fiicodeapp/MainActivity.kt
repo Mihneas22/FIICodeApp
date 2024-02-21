e6f3bdca9e2d847de1a08fc9d2fe9d2c528ff6fd
Mihneas22 <mihnea.serban06@gmail.com>
2024-02-21 20:15:39 +0200

    DeleteTestAPI

--- app/src/main/java/com/example/fiicodeapp/MainActivity.kt
+++ app/src/main/java/com/example/fiicodeapp/MainActivity.kt
@@ -4,37 +4,20 @@ import android.os.Build
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.activity.viewModels
 import androidx.annotation.RequiresExtension
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.tooling.preview.Preview
-import androidx.lifecycle.ViewModel
-import androidx.lifecycle.ViewModelProvider
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
-import com.example.fiicodeapp.di.AppModule
-import com.example.fiicodeapp.features.domain.repository.ProductRepositoryIMPL
 import com.example.fiicodeapp.features.presentation.screens.HomeScreen
 import com.example.fiicodeapp.features.presentation.screens.LoginInScreen
-import com.example.fiicodeapp.features.presentation.screens.MainScreen
 import com.example.fiicodeapp.features.presentation.screens.SignUpScreen
-import com.example.fiicodeapp.features.presentation.viewmodels.ProductsViewModel
 import com.example.fiicodeapp.ui.theme.FIICodeAppTheme
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
 class MainActivity : ComponentActivity() {
-
-    private val viewModel by viewModels<ProductsViewModel>(factoryProducer = {
-        object : ViewModelProvider.Factory {
-            @RequiresExtension(extension = Build.VERSION_CODES.S, version = 7)
-            override fun <T : ViewModel> create(modelClass: Class<T>): T {
-                return ProductsViewModel(ProductRepositoryIMPL(AppModule.api))
-                        as T
-            }
-        }
-    })
     @RequiresExtension(extension = Build.VERSION_CODES.S, version = 7)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -52,10 +35,6 @@ class MainActivity : ComponentActivity() {
                     
                     composable("LoginInScreen"){
                         LoginInScreen(navController = navController)
-                    }
-
-                    composable("ProductsScreen"){
-                        MainScreen(productsViewModel = viewModel)
                     }
                 }
             }
--- app/src/main/java/com/example/fiicodeapp/di/AppModule.kt
+++ app/src/main/java/com/example/fiicodeapp/di/AppModule.kt
@@ -1,24 +1,17 @@
 package com.example.fiicodeapp.di
 
-import com.example.fiicodeapp.features.data.repository.ApiRepository
 import com.example.fiicodeapp.features.data.repository.AuthRepository
-import com.example.fiicodeapp.features.data.repository.ProductsRepository
 import com.example.fiicodeapp.features.domain.models.User
 import com.example.fiicodeapp.features.domain.repository.AuthRepositoryIMPL
-import com.example.fiicodeapp.features.domain.repository.ProductRepositoryIMPL
 import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
-import dagger.hilt.android.components.ActivityComponent
+import dagger.hilt.components.SingletonComponent
 import io.realm.kotlin.Realm
 import io.realm.kotlin.RealmConfiguration
-import okhttp3.OkHttpClient
-import okhttp3.logging.HttpLoggingInterceptor
-import retrofit2.Retrofit
-import retrofit2.converter.gson.GsonConverterFactory
 
 @Module
-@InstallIn(ActivityComponent::class)
+@InstallIn(SingletonComponent::class)
 object AppModule {
 
     @Provides
@@ -30,25 +23,5 @@ object AppModule {
                 )
             )
         )
-    )
-
-    private val interceptor: HttpLoggingInterceptor = HttpLoggingInterceptor().apply {
-        level = HttpLoggingInterceptor.Level.BODY
-    }
-
-    private val client: OkHttpClient = OkHttpClient.Builder()
-        .addInterceptor(interceptor)
-        .build()
-
-    val api: ApiRepository = Retrofit.Builder()
-        .addConverterFactory(GsonConverterFactory.create())
-        .baseUrl(ApiRepository.BASE_URL)
-        .client(client)
-        .build()
-        .create(ApiRepository::class.java)
-
-    @Provides
-    fun provideProductsRepository(): ProductsRepository = ProductRepositoryIMPL(
-        api = api
     )
 }
