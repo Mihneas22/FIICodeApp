28103ef787f0c80dd852a837a852cf3a88ce9e38
Mihneas22 <mihnea.serban06@gmail.com>
2024-02-26 17:46:01 +0200

    Update AppModule.kt

--- app/src/main/java/com/example/fiicodeapp/di/AppModule.kt
+++ app/src/main/java/com/example/fiicodeapp/di/AppModule.kt
@@ -10,10 +10,12 @@ import dagger.hilt.InstallIn
 import dagger.hilt.components.SingletonComponent
 import io.realm.kotlin.Realm
 import io.realm.kotlin.RealmConfiguration
+import javax.inject.Singleton
 
 @Module
 @InstallIn(SingletonComponent::class)
 object AppModule {
+    @Singleton
     @Provides
     fun provideAuthRepository(): AuthRepository = AuthRepositoryIMPL(
         realm = Realm.open(
